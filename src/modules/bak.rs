a75cf9065857031df8415731573399985493ffc2
Gaoyang Zhang <gy@blurgy.xyz>
2021-06-30 16:41:21 +0800

    Bump version to 1.0.1:
    Fix absolute path parsing for utility "bak"
    
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- src/modules/bak.rs
+++ src/modules/bak.rs
@@ -1,7 +1,12 @@
 use std::{
-    fs::rename,
+    fs::{canonicalize, rename},
     path::{Path, PathBuf},
 };
+
+pub fn get_target(path: PathBuf) -> PathBuf {
+    return Path::new(&(path.to_str().unwrap().to_owned() + "~"))
+        .to_path_buf();
+}
 
 pub fn bak(sources: &mut Vec<PathBuf>, quiet: bool) -> Result<(), String> {
     // Abort on empty input.
@@ -25,13 +30,15 @@ pub fn bak(sources: &mut Vec<PathBuf>, quiet: bool) -> Result<(), String> {
         return Err("Invalid path(s)".to_string());
     }
 
+    // Canonicalize to absolute paths.
+    for source in sources.iter_mut() {
+        *source = canonicalize(&source).unwrap();
+    }
+
     // Check for possible confliction of target paths.
     let mut invalid_targets: Vec<PathBuf> = Vec::new();
     for source in sources.iter() {
-        let target = Path::new(
-            &(source.file_name().unwrap().to_str().unwrap().to_owned() + "~"),
-        )
-        .to_path_buf();
+        let target = get_target(source.to_path_buf());
         if target.exists() {
             invalid_targets.push(source.to_path_buf());
         }
@@ -47,10 +54,7 @@ pub fn bak(sources: &mut Vec<PathBuf>, quiet: bool) -> Result<(), String> {
 
     // Actual renaming process.
     for source in sources.iter_mut() {
-        let target = Path::new(
-            &(source.file_name().unwrap().to_str().unwrap().to_owned() + "~"),
-        )
-        .to_path_buf();
+        let target = get_target(source.to_path_buf());
         match rename(&source, &target) {
             Ok(()) => {
                 if !quiet {
