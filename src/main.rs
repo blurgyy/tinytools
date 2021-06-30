52913f7da0ed77857b6aed2856b4c289b81b3aef
Gaoyang Zhang <gy@blurgy.xyz>
2021-06-30 13:43:57 +0800

    Bump version to 1.0.0:
    Use `tt` as entrance of various utilities, so as to reduce installed
    size.  Previous executables are organized as subcommands.  `tt` stands
    for "Tiny Tools".
    
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+mod modules;
+
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
+use modules::{bak::bak, gr::gr};
+
+#[derive(StructOpt)]
+#[structopt(name = "tinytools")]
+enum TT {
+    #[structopt(name = "bak")]
+    /// Append a tilde (~) to the names of given files/directories.
+    Bak {
+        #[structopt(
+            help = "The file(s)/director(y/ies) to append tilde.",
+            parse(from_os_str)
+        )]
+        sources: Vec<PathBuf>,
+        #[structopt(long = "--quiet", short = "-q", help = "Be quiet.")]
+        quiet: bool,
+    },
+    #[structopt(name = "gr")]
+    /// Get nearst git root above current working directory (if it exists).
+    Gr {},
+}
+
+fn main() -> Result<(), String> {
+    match TT::from_args() {
+        TT::Bak { mut sources, quiet } => {
+            bak(&mut sources, quiet)?;
+        }
+        TT::Gr {} => {
+            gr()?;
+        }
+    }
+    Ok(())
+}
+// Author: Blurgy <gy@blurgy.xyz>
+// Date:   Jun 30 2021, 12:39 [CST]
