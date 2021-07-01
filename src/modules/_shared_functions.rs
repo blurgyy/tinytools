c158c4dd1acc6b6fd03eea003fd9b7e59d44514c
Gaoyang Zhang <gy@blurgy.xyz>
2021-07-02 01:01:41 +0800

    Add utility "debak" to pop a tilde (~) to given path(s)
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

---
+++ src/modules/_shared_functions.rs
@@ -0,0 +1,34 @@
+use std::path::PathBuf;
+
+/// Validates a vector of paths.
+///
+/// Returns error on:
+///     - Empty input vector
+///     - Non-existing path in input vector
+pub fn validate_paths(sources: Vec<PathBuf>) -> Result<(), String> {
+    // Abort on empty input.
+    if sources.len() == 0 {
+        return Err("Missing input path(s)".to_string());
+    }
+
+    // Check for validity of given source paths.
+    let mut invalid_sources: Vec<PathBuf> = Vec::new();
+    for source in sources.iter() {
+        if !source.is_file() && !source.is_dir() {
+            invalid_sources.push(source.to_path_buf());
+        }
+    }
+    if invalid_sources.len() > 0 {
+        eprintln!(
+            "The following input paths are invalid:\n{:#?}",
+            invalid_sources
+        );
+        eprintln!("Nothing done");
+        return Err("Invalid path(s)".to_string());
+    }
+
+    Ok(())
+}
+
+// Author: Blurgy <gy@blurgy.xyz>
+// Date:   Jul 02 2021, 00:06 [CST]
